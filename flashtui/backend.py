2363a6671a3f2b0c463cf23ac3ad82d10150e117
nautrw <nautrw@proton.me>
2025-06-23 07:55:36 -0400

    change return type of read_deck_from_text

--- flashtui/backend.py
+++ flashtui/backend.py
@@ -23,11 +23,8 @@ def read_config(path: str) -> dict:
         return json.loads(file)
 
 
-def read_deck_from_text(format: str, text: str) -> List[str]:
+def read_deck_from_text(format: str, text: str) -> List[tuple]:
     separator = format.replace('{term}', '').replace('{definition}', '')
     split_list = text.split(separator)
     deck_pairs = pairwise(split_list)
     return list(deck_pairs)
-
-
-print(read_deck_from_text('{term} - {definition}', 'a - b'))
