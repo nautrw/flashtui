ccead725132c6e90a880da638e7dce0995530e0e
nautrw <nautrw@proton.me>
2025-06-23 09:16:08 -0400

    format backend.py

--- flashtui/backend.py
+++ flashtui/backend.py
@@ -4,19 +4,19 @@ from typing import List
 
 
 def create_config(path: str) -> None:
-    '''
+    """
     Creates the configuration file for the program
-    '''
+    """
 
-    with open(path, 'w+') as file:
+    with open(path, "w+") as file:
         if not file.read():
             file.write('{"decks":{}}')
 
 
 def read_config(path: str) -> dict:
-    '''
+    """
     Returns the configuration data in the configuration file
-    '''
+    """
 
     with open(path, 'r') as file:
         file = file.read()
@@ -24,7 +24,7 @@ def read_config(path: str) -> dict:
 
 
 def read_deck_from_text(format: str, text: str) -> List[tuple]:
-    separator = format.replace('{term}', '').replace('{definition}', '')
+    separator = format.replace("{term}", "").replace("{definition}", "")
     split_list = text.split(separator)
     deck_pairs = pairwise(split_list)
     return list(deck_pairs)
